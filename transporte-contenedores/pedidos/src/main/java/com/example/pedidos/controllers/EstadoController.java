206c8d9043ae42fe8b6ccd90b4a131857587758f
Nicolas Pereira <nicopereira2802@gmail.com>
2025-08-20 21:36:00 -0300

    estado controller mod

--- transporte-contenedores/pedidos/src/main/java/com/example/pedidos/controllers/EstadoController.java
+++ transporte-contenedores/pedidos/src/main/java/com/example/pedidos/controllers/EstadoController.java
@@ -9,11 +9,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.pedidos.services.EstadoService;
 
 import com.example.pedidos.models.Estado;
 
+@RestController
+@RequestMapping("/pedidos/estados")
 public class EstadoController {
 
     @Autowired
